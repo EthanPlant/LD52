4d6e57f912155c75a70eda51762291c9d756b68e
Ethan Plant <plant.ethan@gmail.com>
2023-01-08 07:38:55 -0500

    Clean up scene tree a bit

--- world.gd
+++ world.gd
@@ -3,14 +3,13 @@ extends Node2D
 var inventory_open = false
 
 func _physics_process(delta):
-	$coin_text.text = (" " + str(Global.coins))
+	$ui/coin_text.text = (" " + str(Global.coins))
 
 func _input(event):
 	if event.is_action_pressed("inventory"):
-		print("Hi")
 		if inventory_open:
-			$inventory.visible = false
+			$ui/inventory.visible = false
 			inventory_open = false
 		else:
-			$inventory.visible = true
+			$ui/inventory.visible = true
 			inventory_open = true
